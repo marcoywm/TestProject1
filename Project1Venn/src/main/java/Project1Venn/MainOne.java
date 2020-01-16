1173d31cd0507a24308bba71ffbd9b8fa04988fe
marcoy2m <marcoy2m@gmail.com>
2020-01-16 17:14:20 -0500

    TestVerion2

--- Project1Venn/src/main/java/Project1Venn/MainOne.java
+++ Project1Venn/src/main/java/Project1Venn/MainOne.java
@@ -5,6 +5,9 @@ public class MainOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("one");
+		System.out.println("Hello World!");
+		System.out.println("How are you?");
+		System.out.println("Test1?");
 	}
 
 }
